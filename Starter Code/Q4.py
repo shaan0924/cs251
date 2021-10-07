357486a6e9e03156d7823f2a03daa0c11bfb6f07
Shaan Patel <shaan98@stanford.edu>
2021-10-06 21:50:05 -0700

    Q4 ready to debug

--- Starter Code/Q4.py
+++ Starter Code/Q4.py
@@ -14,19 +14,19 @@ from bitcoin.core.script import *
 # This is the ScriptPubKey for the swap transaction
 def coinExchangeScript(public_key_sender, public_key_recipient, hash_of_secret):
     return [
-        # fill this in!
+        OP_DUP, OP_HASH160, hash_of_secret, OP_EQUAL, OP_IF, OP_DROP, public_key_sender, OP_CHECKSIG, OP_ELSE, public_key_recipient, OP_CHECKSIGVERIFY, public_key_sender, OP_CHECKSIG, OP_ENDIF
     ]
 
 # This is the ScriptSig that the receiver will use to redeem coins
 def coinExchangeScriptSig1(sig_recipient, secret):
     return [
-        # fill this in!
+        sig_recipient, secret
     ]
 
 # This is the ScriptSig for sending coins back to the sender if unredeemed
 def coinExchangeScriptSig2(sig_sender, sig_recipient):
     return [
-        # fill this in!
+        sig_sender, sig_recipient
     ]
 ######################################################################
 
